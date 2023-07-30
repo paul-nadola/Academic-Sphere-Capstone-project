6da9ebe68cedcd7a3cb088e5e63e9e8af80c3a34
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-07-30 15:33:25 +0300

    model and virtual env requirements

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -9,11 +9,11 @@ function Hero() {
     <section className='hero' onMouseOver={closeSubmenu}>
       <div className='hero-center'>
         <article className='hero-info'>
-            <h1>Welcome to Prudent Group of Schools</h1>
-            <p>Prudent Academy (Pri) School is a private primary school in Orlando, Florida.
-            This school is run by private individual. It is a day school. This educational institution is a ordinary and mixed type of institution.
-            The pupil to classroom ratio in this school is 14.6:1 and the pupil to toilet ratio is 33.3:1. There are total 16 classrooms, 3 boys toilets, 4 girls toilets and 1 teachers toilet. 
-            There are 114 boys and 119 girls in Prudent Academy (Pri) School. </p>
+            <h1>Welcome to Academic Sphere</h1>
+            <p>Academic Sphere is a student management system for schools to keep track of student's and teacher's
+              performance, attendance and relevant information and also send relevant information to parents
+              with children and/or students they are sponsoring.
+                        </p>
             <button className='btn'>Join Us</button>
         </article>
         <article className='hero-images'>
