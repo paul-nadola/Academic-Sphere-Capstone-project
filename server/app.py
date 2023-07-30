49305d37941baa7b068b951ddb29c931161b11f3
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-07-30 16:51:42 +0300

    user and profile creation

--- server/app.py
+++ server/app.py
@@ -1,13 +1,17 @@
+import os
 from flask_cors import CORS
-
+from flask import Flask, jsonify, session,request, make_response
 from config import app, db
 
 CORS(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+
 
 
 @app.route('/')
 def index():
-    return f'welcome'
+    # return f'welcome'
+    return jsonify(detail = 'Welcome, this is a test route')
 
 
 if __name__ == "__main__":
--- server/models.py
+++ server/models.py
@@ -1,177 +1,99 @@
-# from sqlalchemy.ext.hybrid import hybrid_property
-# from sqlalchemy import Enum
-
-# from config import db, bcrypt
+from sqlalchemy_serializer import SerializerMixin
+from config import db
 
 
-# class UserWithPassword(db.Model):
-#     __abstract__ = True
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.String, nullable=False)
-#     _password_hash = db.Column(db.String, nullable=False)
+class User(db.Model, SerializerMixin):
 
-#     @hybrid_property
-#     def password_hash(self):
-#         return self._password_hash
+    __tablename__ = 'users'
 
-#     @password_hash.setter
-#     def password_hash(self, password):
-#         hashed_password = bcrypt.generate_password_hash(
-#             password.encode('utf-8'))
-#         self._password_hash = hashed_password.decode('utf-8')
-
-#     def authenticate(self, password):
-#         return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
-
-
-# class Admin(UserWithPassword):
-#     __tablename__ = "admins"
-
-#     tel = db.Column(db.Integer, nullable=False)
-
-#     def __repr__(self):
-#         return f'Fist Name: {self.first_name}'
-
-
-# guardian_student = db.Table(
-#     'guardian_student',
-#     db.Column('guardian_id', db.Integer, db.ForeignKey(
-#         'guardians.id'), primary_key=True),
-#     db.Column('student_id', db.Integer, db.ForeignKey(
-#         'students.id'), primary_key=True)
-# )
-
-
-# class Guardian(UserWithPassword):
-#     __tablename__ = "guardians"
-
-#     tel = db.Column(db.Integer, nullable=False)
-
-#     children = db.relationship(
-#         'Student', secondary=guardian_student, back_populates='guardian')
-
-#     def __repr__(self):
-#         return f'Fist Name: {self.first_name}'
-
-
-# class Student(UserWithPassword):
-#     __tablename__ = "students"
-
-#     dob = db.Column(db.Date, nullable=False)
-#     enrolment_date = db.Column(db.Date, nullable=False)
-#     fee_balance = db.Column(db.Integer)
-#     amount_paid = db.Column(db.Integer)
-#     course_id = db.Column(db.Integer, db.ForeignKey('courses.id'))
-#     teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'))
-
-#     guardians = db.relationship(
-#         'Guardian', secondary=guardian_student, back_populates='student')
-
-#     def __repr__(self):
-#         return f'Fist Name: {self.first_name}'
-
-
-# class Teacher(UserWithPassword):
-#     __tablename__ = "teachers"
-
-#     tel = db.Column(db.Integer, nullable=False)
-#     course_id = db.Column(db.Integer, db.ForeignKey('courses.id'))
-#     department_id = db.Column(db.Integer, db.ForeignKey('departments.id'))
-
-#     students = db.relationship('Student', backref='teacher')
-
-#     def __repr__(self):
-#         return f'Fist Name: {self.first_name}'
-
-
-# class Course(db.Model):
-#     __tablename__ = 'courses'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String, nullable=False)
-#     department_id = db.Column(db.Integer, db.ForeignKey('departments.id'))
-
-#     students = db.relationship('Student', backref='course')
-#     teachers = db.relationship('Teacher', backref='course')
-
-
-# class Department(db.Model):
-#     __tablename__ = "departments"
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String, nullable=False)
-
-#     courses = db.relationship('Course', backref='department')
-#     teachers = db.relationship('Teacher', backref='department')
-
-
-# class Payroll(db.Model):
-#     __tablename__ = "payrolls"
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'))
-#     salary = db.Column(db.Integer, nullable=False)
-
-#     teacher = db.relationship('Teacher', backref='salary')
-
-
-# class Leave(db.Model):
-#     __tablename__ = 'leaves'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     start_date = db.Column(db.Date)
-#     end_date = db.Column(db.Date)
-#     teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'))
-
-#     teacher = db.relationship('Teacher', backref='leave')
-
-
-# class StudentAttendace(db.Model):
-#     __tablename__ = "student_attendance"
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     date = db.Column(db.Date, nullable=False)
-#     present = db.Column(db.Boolean, default=False)
-#     student_id = db.Column(db.Integer, db.ForeignKey(
-#         'students.id'), nullable=False)
-
-#     student = db.relationship(
-#         'Student', backref=db.backref('attendance', lazy=True))
-
-
-# class TeacherAttendance(db.Model):
-#     __tablename__ = "teacher_attendance"
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     date = db.Column(db.Date, nullable=False)
-#     present = db.Column(db.Boolean, default=False)
-#     teacher_id = db.Column(db.Integer, db.ForeignKey(
-#         'teachers.id'), nullable=False)
-
-#     teacher = db.relationship(
-#         'Teacher', backref=db.backref('attendance', lazy=True))
-
-
-# class OtherExpenses(db.Model):
-#     __tablename__ = 'expenses'
-
-#     id = db.Column(db.Integer, primary_key=True)
-
-#     expense_name = db.Column(db.String, nullable=False)
-#     amount = db.Column(db.Integer, nullable=False)
-
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(40), index = True ,unique = True ,nullable=False)
     email = db.Column(db.String(40), index = True ,unique = True , nullable=False)
     password = db.Column(db.String(65), nullable=False)
     user_type = db.Column(db.String(40))
 
-# class Student(db.Model):
-#     student_id = db.Column(db.Integer, primary_key=True)
-#     user_id = db.Column(db.Integer, Forei
+    student = db.relationship('Students', backref = 'users', uselist = False)
+    parent = db.relationship('Parents', backref = 'users', uselist = False)
+    teacher = db.relationship('Teachers', backref = 'users', uselist = False)
+    admin = db.relationship('Admin', backref = 'users', uselist = False)
+
+    def __repr__(self):
+        return f'<User {self.user_name} | Type: {self.user_type}>'
+
+class Students(db.Model, SerializerMixin):
+
+    __tablename__ = 'Students'
+
+    student_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    student_name = db.Column(db.String(255), nullable=False)
+    DOB = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    phone_number = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    enrollment_date = db.Column(db.String(100), nullable=False)
+    department = db.Column(db.String(100), nullable=False)
+    course = db.Column(db.String(100), nullable=False)
+    grade = db.Column(db.String(100), nullable=False)
+    parent_guardian = db.relationship('Parents', backref = 'Students')
+    teacher = db.relationship('Teachers', backref = 'Students')
+
+    def __repr__(self):
+        return f'<Student {self.student_name} | ID: {self.student_id}>'
+
+
+class Parents(db.Model, SerializerMixin):
+
+    __tablename__ = 'Parents'
+
+    parent_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    student_id = db.Column(db.Integer, db.ForeignKey('Students.id'))
+    parent_name = db.Column(db.String(255), nullable=False)
+    DOB = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    phone_number = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f'<Parent {self.parent_name} | ID: {self.parent_id}>'
+    
+class Teachers(db.Model, SerializerMixin):
+
+    __tablename__ = 'Teachers'
+
+    teacher_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    student_id = db.Column(db.Integer, db.ForeignKey('Students.id'))
+    teacher_name = db.Column(db.String(255), nullable=False)
+    DOB = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    phone_number = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    employment_date = db.Column(db.String(100), nullable=False)
+    department = db.Column(db.String(100), nullable=False)
+    course = db.Column(db.String(100), nullable=False)
+    appraisal = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Teacher {self.teacher_name} | ID: {self.teacher_id}>'
+
+
+class Admin(db.Model, SerializerMixin):
+
+    __tablename__ = 'Admin'
+
+    admin_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    admin_name = db.Column(db.String(255), nullable=False)
+    DOB = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    phone_number = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    employment_date = db.Column(db.String(100), nullable=False)
+    appraisal = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Admin {self.admin_name} | ID: {self.admin_id}>'
+
