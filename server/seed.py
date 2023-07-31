9ff38879507f9806a30547a47c29e8e8a2f9227a
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-07-31 15:46:01 +0300

    first route

--- server/seed.py
+++ server/seed.py
@@ -1,261 +1,150 @@
-# from faker import Faker
-# from config import db, app
-# from models import User, Students, Parents, Teachers, Admin
+from app import app
 
-# fake = Faker()
+from flask_bcrypt import Bcrypt
+import random
+import string
+from models import db, User, Admin,SuperAdmin,Parents,Students,Teachers
+bcrypt = Bcrypt()
 
-# def create_users():
-#     for _ in range(10):
-#         user_name = fake.user_name()
-#         email = fake.email()
-#         password = fake.password()
-#         user_type = fake.random_element(elements=("student", "parent", "teacher", "admin"))
+def generate_random_string(length=10):
+    return ''.join(random.choices(string.ascii_letters, k=length))
 
-#         user = User(user_name=user_name, email=email, password=password, user_type=user_type)
-#         db.session.add(user)
-#     db.session.commit()
+def create_seed_data():
+    # Create database tables
+    db.create_all()
 
-# def create_students():
-#     users = User.query.filter_by(user_type='student').all()
-#     for user in users:
-#         student_name = fake.name()
-#         dob = fake.date_of_birth()
-#         address = fake.address()
-#         phone_number = fake.phone_number()
-#         email = fake.email()
-#         enrollment_date = fake.date_between(start_date='-1y', end_date='today')
-#         department = fake.random_element(elements=("Science", "Arts", "Commerce"))
-#         course = fake.random_element(elements=("Mathematics", "English", "History"))
-#         grade = fake.random_element(elements=("A", "B", "C"))
-
-#         student = Students(
-#             user_id=user.id,
-#             student_name=student_name,
-#             DOB=dob,
-#             address=address,
-#             phone_number=phone_number,
-#             email=email,
-#             enrollment_date=enrollment_date,
-#             department=department,
-#             course=course,
-#             grade=grade
-#         )
-#         db.session.add(student)
-#     db.session.commit()
-
-# def create_parents():
-#     users = User.query.filter_by(user_type='parent').all()
-#     students = Students.query.all()
-#     for user in users:
-#         student = fake.random_element(elements=students)
-#         parent_name = fake.name()
-#         dob = fake.date_of_birth()
-#         address = fake.address()
-#         phone_number = fake.phone_number()
-#         email = fake.email()
-
-#         parent = Parents(
-#             user_id=user.id,
-#             student_id=student.student_id,
-#             parent_name=parent_name,
-#             DOB=dob,
-#             address=address,
-#             phone_number=phone_number,
-#             email=email
-#         )
-#         db.session.add(parent)
-#     db.session.commit()
-
-# def create_teachers():
-#     users = User.query.filter_by(user_type='teacher').all()
-#     students = Students.query.all()
-#     for user in users:
-#         student = fake.random_element(elements=students)
-#         teacher_name = fake.name()
-#         dob = fake.date_of_birth()
-#         address = fake.address()
-#         phone_number = fake.phone_number()
-#         email = fake.email()
-#         employment_date = fake.date_between(start_date='-5y', end_date='today')
-#         department = fake.random_element(elements=("Science", "Arts", "Mathematics"))
-#         course = fake.random_element(elements=("Physics", "Chemistry", "Biology"))
-#         appraisal = fake.random_int(min=1, max=5)
-
-#         teacher = Teachers(
-#             user_id=user.id,
-#             student_id=student.student_id,
-#             teacher_name=teacher_name,
-#             DOB=dob,
-#             address=address,
-#             phone_number=phone_number,
-#             email=email,
-#             employment_date=employment_date,
-#             department=department,
-#             course=course,
-#             appraisal=appraisal
-#         )
-#         db.session.add(teacher)
-#     db.session.commit()
-
-# def create_admins():
-#     users = User.query.filter_by(user_type='admin').all()
-#     for user in users:
-#         admin_name = fake.name()
-#         dob = fake.date_of_birth()
-#         address = fake.address()
-#         phone_number = fake.phone_number()
-#         email = fake.email()
-#         employment_date = fake.date_between(start_date='-2y', end_date='today')
-#         appraisal = fake.random_int(min=1, max=5)
-
-#         admin = Admin(
-#             user_id=user.id,
-#             admin_name=admin_name,
-#             DOB=dob,
-#             address=address,
-#             phone_number=phone_number,
-#             email=email,
-#             employment_date=employment_date,
-#             appraisal=appraisal
-#         )
-#         db.session.add(admin)
-#     db.session.commit()
-
-# if __name__ == '__main__':
-#     with app.app_context():
-#         create_users()
-#         create_students()
-#         create_parents()
-#         create_teachers()
-#         create_admins()
-from faker import Faker
-from config import db, app
-from models import User, Students, Parents, Teachers, Admin
-
-fake = Faker()
-app.app_context().push()
-
-def create_users():
-    for _ in range(10):
-        user_name = fake.user_name()
-        email = fake.email()
-        password = fake.password()
-        user_type = fake.random_element(elements=("student", "parent", "teacher", "admin"))
-
-        user = User(user_name=user_name, email=email, password=password, user_type=user_type)
-        db.session.add(user)
+    # Create SuperAdmin
+    super_admin_user = User(
+        user_name='superadmin',
+        email='superadmin@example.com',
+        password=bcrypt.generate_password_hash(generate_random_string()).decode('utf-8'),
+        user_type='superadmin',
+    )
+    db.session.add(super_admin_user)
+    db.session.commit()
+    super_admin = SuperAdmin(
+        user_id=super_admin_user.id,
+        first_name='John',
+        last_name='Doe',
+        DOB='1990-01-01',
+        address='123 Main Street',
+        phone_number='123-456-7890',
+        email='john.doe@example.com',
+        employment_date='2020-01-01',
+    )
+    db.session.add(super_admin)
     db.session.commit()
 
-def create_students():
-    users = User.query.filter_by(user_type='student').all()
-    for user in users:
-        student_name = fake.name()
-        dob = fake.date_of_birth()
-        address = fake.address()
-        phone_number = fake.phone_number()
-        email = fake.email()
-        enrollment_date = fake.date_between(start_date='-1y', end_date='today')
-        department = fake.random_element(elements=("Science", "Arts", "Commerce"))
-        course = fake.random_element(elements=("Mathematics", "English", "History"))
-        grade = fake.random_element(elements=("A", "B", "C"))
-
+    # Create Students
+    students = []
+    for i in range(1, 11):
+        user = User(
+            user_name=f'student{i}',
+            email=f'student{i}@example.com',
+            password=bcrypt.generate_password_hash(generate_random_string()).decode('utf-8'),
+            user_type='student',
+        )
+        db.session.add(user)
+        db.session.commit()
         student = Students(
             user_id=user.id,
-            student_name=student_name,
-            DOB=dob,
-            address=address,
-            phone_number=phone_number,
-            email=email,
-            enrollment_date=enrollment_date,
-            department=department,
-            course=course,
-            grade=grade
+            first_name=f'Student{i}',
+            last_name=f'Last{i}',
+            DOB='2000-01-01',
+            address=f'Student{i} Address',
+            phone_number=f'123-456-789{i}',
+            email=f'student{i}@example.com',
+            enrollment_date='2022-01-01',
+            department=f'Department{i}',
+            course=f'Course{i}',
+            grade=f'A',
         )
+        students.append(student)
         db.session.add(student)
-    db.session.commit()
+        db.session.commit()
 
-def create_parents():
-    users = User.query.filter_by(user_type='parent').all()
-    students = Students.query.all()
-    for user in users:
-        student = fake.random_element(elements=students)
-        parent_name = fake.name()
-        dob = fake.date_of_birth()
-        address = fake.address()
-        phone_number = fake.phone_number()
-        email = fake.email()
-
+    # Create Parents
+    parents = []
+    for i in range(1, 6):
+        user = User(
+            user_name=f'parent{i}',
+            email=f'parent{i}@example.com',
+            password=bcrypt.generate_password_hash(generate_random_string()).decode('utf-8'),
+            user_type='parent',
+        )
+        db.session.add(user)
+        db.session.commit()
         parent = Parents(
             user_id=user.id,
-            student_id=student.student_id,
-            parent_name=parent_name,
-            DOB=dob,
-            address=address,
-            phone_number=phone_number,
-            email=email
+            first_name=f'Parent{i}',
+            last_name=f'Last{i}',
+            DOB='1980-01-01',
+            address=f'Parent{i} Address',
+            phone_number=f'987-654-321{i}',
+            email=f'parent{i}@example.com',
+            student_id=random.choice(students)
         )
+        parents.append(parent)
         db.session.add(parent)
     db.session.commit()
 
-def create_teachers():
-    users = User.query.filter_by(user_type='teacher').all()
-    students = Students.query.all()
-    for user in users:
-        student = fake.random_element(elements=students)
-        teacher_name = fake.name()
-        dob = fake.date_of_birth()
-        address = fake.address()
-        phone_number = fake.phone_number()
-        email = fake.email()
-        employment_date = fake.date_between(start_date='-5y', end_date='today')
-        department = fake.random_element(elements=("Science", "Arts", "Mathematics"))
-        course = fake.random_element(elements=("Physics", "Chemistry", "Biology"))
-        appraisal = fake.random_int(min=1, max=5)
-
+    # Create Teachers
+    for i in range(1, 6):
+        user = User(
+            user_name=f'teacher{i}',
+            email=f'teacher{i}@example.com',
+            password=bcrypt.generate_password_hash(generate_random_string()).decode('utf-8'),
+            user_type='teacher',
+        )
+        db.session.add(user)
+        db.session.commit()
         teacher = Teachers(
             user_id=user.id,
-            student_id=student.student_id,
-            teacher_name=teacher_name,
-            DOB=dob,
-            address=address,
-            phone_number=phone_number,
-            email=email,
-            employment_date=employment_date,
-            department=department,
-            course=course,
-            appraisal=appraisal
+            first_name=f'Teacher{i}',
+            last_name=f'Last{i}',
+            DOB='1985-01-01',
+            address=f'Teacher{i} Address',
+            phone_number=f'555-555-555{i}',
+            email=f'teacher{i}@example.com',
+            employment_date='2021-01-01',
+            department=f'Department{i}',
+            course=f'Course{i}',
+            appraisal=random.randint(1, 5),
+            student_id=random.choice(students)
         )
         db.session.add(teacher)
     db.session.commit()
 
-def create_admins():
-    users = User.query.filter_by(user_type='admin').all()
-    for user in users:
-        admin_name = fake.name()
-        dob = fake.date_of_birth()
-        address = fake.address()
-        phone_number = fake.phone_number()
-        email = fake.email()
-        employment_date = fake.date_between(start_date='-2y', end_date='today')
-        appraisal = fake.random_int(min=1, max=5)
-
+    # Create Admins
+    for i in range(1, 6):
+        user = User(
+            user_name=f'admin{i}',
+            email=f'admin{i}@example.com',
+            password=bcrypt.generate_password_hash(generate_random_string()).decode('utf-8'),
+            user_type='admin',
+        )
+        db.session.add(user)
+        db.session.commit()
         admin = Admin(
             user_id=user.id,
-            admin_name=admin_name,
-            DOB=dob,
-            address=address,
-            phone_number=phone_number,
-            email=email,
-            employment_date=employment_date,
-            appraisal=appraisal
+            first_name=f'Admin{i}',
+            last_name=f'Last{i}',
+            DOB='1975-01-01',
+            address=f'Admin{i} Address',
+            phone_number=f'111-222-333{i}',
+            email=f'admin{i}@example.com',
+            employment_date='2020-01-01',
+            appraisal=random.randint(1, 5),
         )
         db.session.add(admin)
+
+    # Commit changes to the database
     db.session.commit()
 
 if __name__ == '__main__':
-    create_users()
-    create_students()
-    create_parents()
-    create_teachers()
-    create_admins()
-
+    with app.app_context():
+    # Replace 'your_flask_app' with the name of your Flask app instance
+    # Make sure to have your Flask app initialized before running this script
+    # Example: from your_flask_app import app
+    # Note: The app instance should be imported before creating the seed data.
+        create_seed_data()
