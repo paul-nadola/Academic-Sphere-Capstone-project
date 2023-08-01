63ca7003924095dbd9a2eae922ee262fff0897cf
manotim <manotimike@gmail.com>
2023-08-01 14:55:16 +0300

    navbar refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,11 @@
 import React from 'react'
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Sidebar from './components/Sidebar';
-import SubMenu from './components/SubMenu';
 import './App.css';
 
 function App() {
   return (
     <>
       <Navbar/>
-      <Sidebar/>
-      <Hero/>
-      <SubMenu/>
     </>
   )
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,53 +1,39 @@
 import React from 'react'
-import logo from '../images/logo.svg'
 import { FaBars } from 'react-icons/fa'
-import { useGlobalContext } from './context'
+
 
 function Navbar() {
-  const {openSidebar, openSubmenu, closeSubmenu} = useGlobalContext()
-  const displaySubmenu = (e) => {
-    e.preventDefault();
-    const page = e.target.textContent
-    const tempBtn = e.target.getBoundingClientRect()
-    const center = (tempBtn.left + tempBtn.right) / 2
-    const bottom = tempBtn.bottom - 3
+  
+  
 
-    openSubmenu(page, {center, bottom})
-
-  }
-
-  const handleSubmenu = (e) => {
-    if(!e.target.classList.contains('link-btn')) {
-      setTimeout(()=>closeSubmenu(),10);
-    }
-  }
+  
   return (
-    <nav className='nav' onMouseOver={handleSubmenu}>
+    <nav className='nav' >
       <div className='nav-center'>
         <div className='nav-header'>
          {/* <img src={logo} className='nav-logo' alt='stripe'/> */}
-         <button className='btn toggle-btn' onClick={openSidebar}>
+         <button className='btn toggle-btn' >
           <FaBars />
          </button>
         </div>
         <ul className='nav-links'>
         <li>
-            <button className='link-btn' onMouseOver={displaySubmenu}>
+            <button className='link-btn' >
               home
             </button>
             </li>
           <li>
-            <button className='link-btn' onMouseOver={displaySubmenu}>
+            <button className='link-btn' >
               admissions
             </button>
             </li>
           <li>
-          <button className='link-btn' onMouseOver={displaySubmenu}>
+          <button className='link-btn' >
               academics
             </button>
           </li>
           <li>
-          <button className='link-btn' onMouseOver={displaySubmenu}>
+          <button className='link-btn' >
               about
             </button>
             </li>           
