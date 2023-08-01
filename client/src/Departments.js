6d04b933412cd30c863779d134883d66a4a305a6
Tomno <tomnoviv@gmail.com>
2023-08-01 15:33:31 +0300

    another commit

--- client/src/Departments.js
+++ client/src/Departments.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Departments() {
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-100">
+    <div className="h-screen bg-cover bg-center flex items-center justify-center">
+      <div className="absolute top-0 right-0 p-4">
+        <Link
+          to="/home"
+          className="block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300"
+        >
+          Home
+        </Link>
+      </div>
       <h1 className="text-4xl font-bold">Departments</h1>
     </div>
   );
