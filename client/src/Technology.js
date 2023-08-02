727c707c1c83cdf54bc0cf67d8f07020c153817f
manotim <manotimike@gmail.com>
2023-08-02 13:48:21 +0300

    Added the Technology page Structure

---
+++ client/src/Technology.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Technology() {
+  return (
+    <div>
+        <section>
+        <h1>Technology Page</h1>
+        <p>Our department is dedicated to fostering a vibrant and enriching learning environment, where students are encouraged to explore their interests, unleash their potential, and achieve academic excellence. As an integral part of Prudent Group of schools, our department plays a pivotal role in shaping the educational journey of our students.</p>
+
+<p>With a rigorous and engaging curriculum, we strive to provide students with a solid foundation in the technological field while encouraging critical thinking, problem-solving, and creativity. We believe in a student-centric approach, tailoring our teaching methods to accommodate various learning styles and abilities.</p>
+        </section>
+        <section>
+            <h2>Courses Offered</h2>
+            <ol>
+                <li>Software Engineering</li>
+                <li>UI/UX Design</li>
+                <li>Data Science</li>
+                <li>Cyber Security</li>
+                <li>Networking</li>
+            </ol>
+        </section>
+        
+    </div>
+  )
+}
+
+export default Technology
