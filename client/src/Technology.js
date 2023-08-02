19696bff80dc206a228d203c01bc6701bd1f60d0
manotim <manotimike@gmail.com>
2023-08-03 00:11:04 +0300

    added the page's navigation links

--- client/src/Technology.js
+++ client/src/Technology.js
@@ -4,7 +4,35 @@ import { Link } from 'react-router-dom';
 function Technology() {
   return (
     <div>
-        <button className="mt-4">
+      <div className="flex justify-evenly">
+            <button className='mt-4'>
+            <Link
+              to="/departments"
+              className="block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300 text-center"
+            >
+              Back To Departments
+            </Link>
+            </button>
+            
+            <button className='mt-4'>
+            <Link
+              to="/parents"
+              className="block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300 text-center"
+            >
+              Parent Page
+            </Link>
+            </button>
+            
+            <button className='mt-4'>
+            <Link
+              to="/about"
+              className="block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300 text-center"
+            >
+              About
+            </Link>
+            </button>
+            
+            <button className="mt-4">
             <Link
               to="/"
               className="block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300 w-full text-center"
@@ -12,6 +40,7 @@ function Technology() {
               Sign Out
             </Link>
           </button>
+          </div>
         <section>
         <h1>Technology Page</h1>
         <p>Our department is dedicated to fostering a vibrant and enriching learning environment, where students are encouraged to explore their interests, unleash their potential, and achieve academic excellence. As an integral part of Prudent Group of schools, our department plays a pivotal role in shaping the educational journey of our students.</p>
