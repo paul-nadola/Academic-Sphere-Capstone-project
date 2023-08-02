392609a679e7cd73133cf825b0ef489e65467730
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-08-02 13:07:49 +0300

    seed data

--- server/seed2.py
+++ server/seed2.py
@@ -4,8 +4,8 @@ import datetime
 
 with app.app_context():
     super_admin_user = User(
-        user_name='superadmin',
-        email='superadmin@example.com',
+        user_name='superadmin2',
+        email='superadmin2@example.com',
         password_hash='1234',
         user_type='superadmin',
     )
@@ -14,11 +14,11 @@ with app.app_context():
     super_admin = SuperAdmin(
         user_id=super_admin_user.id,
         first_name='John',
-        last_name='Doe',
+        last_name='NeDoe',
         DOB=datetime.date(1990, 1, 1),
-        address='123 Main Street',
-        phone_number='123-456-7890',
-        employment_date=datetime.date(2020, 1, 1),
+        address='1234 Main Street',
+        phone_number='123-456-789',
+        employment_date=datetime.date(2021, 1, 1),
     )
     db.session.add(super_admin)
     db.session.commit()
