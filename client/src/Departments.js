989126a853d28db458fede272f66eefa793bab8f
manotim <manotimike@gmail.com>
2023-08-02 13:20:05 +0300

    added some more content to the Departments Page

--- client/src/Departments.js
+++ client/src/Departments.js
@@ -4,6 +4,10 @@ function Departments() {
   return (
     <div className="h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold">Departments</h1>
+      <p>We have three major departments at our school, which cater for the different courses offered. Take a tour around!</p>
+      <button>TECHNOLOGY</button>
+      <button>SOCIAL SCIENCES</button>
+      <button>ENGINEERING</button>
     </div>
   );
 }
