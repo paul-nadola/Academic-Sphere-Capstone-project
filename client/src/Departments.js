28df9fa8e99dbae9d042e377288c3626b39f2755
manotim <manotimike@gmail.com>
2023-08-03 01:30:00 +0300

    updated the component

--- client/src/Departments.js
+++ client/src/Departments.js
@@ -37,12 +37,16 @@ function Departments() {
               to="/signin"
               className="block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300 w-full text-center"
             >
-              Sign In
+              Sign Out
             </Link>
           </button>
           </div>
-    <div className="h-screen flex justify-start bg-gray-500 w-90vw">
-      <div className='bg-red-400 h-70vh w-90vw flex flex-col items-center'>
+    <div className="h-screen flex justify-start bg-gray-500 w-90vw" style={{
+        backgroundImage: `url('https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}>
+      <div className='h-70vh w-90vw flex flex-col items-center'>
       <h1 className="text-4xl font-bold">Departments</h1>
       <p>
         We have three major departments at our school, which cater for the different courses offered. Take a tour around!
