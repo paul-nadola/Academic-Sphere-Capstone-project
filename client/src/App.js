13b263b142976696dc6d3a4154c21f3b833545d1
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-08-03 17:45:13 +0300

    done

--- client/src/App.js
+++ client/src/App.js
@@ -9,16 +9,14 @@ import About from './About';
 import SignIn from './SignIn';
 import ParentsPage from './ParentsPage';
 import StudentsPage from './StudentsPage';
-import React from 'react'
 // import Navbar from './components/Navbar';
 // import Hero from './components/Hero';
 // import Sidebar from './components/Sidebar';
 // import SubMenu from './components/SubMenu';
-import Login from './components/Login';
-import Nav from './components/Nav';
-import {  Route, Routes, useNavigate } from 'react-router-dom'
-import './App.css';
-import Teachers from './components/Teachers';
+// import Login from './components/Login';
+// import Nav from './components/Nav';
+// import './App.css';
+// import Teachers from './components/Teachers';
 
 function App() {
   return (
