d3b90fb438cbc8d52859af8aaecad268299f248a
manotim <manotimike@gmail.com>
2023-08-03 17:53:25 +0300

    Added sign out button

--- client/src/admins/SuperAdmin.js
+++ client/src/admins/SuperAdmin.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function SuperAdmin() {
   return (
     <div>
+        <button className="mt-4">
+            <Link
+              to="/signin"
+              className="block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300 w-full text-center"
+            >
+              Sign Out
+            </Link>
+          </button>
         <h1>Super Admin Page</h1>
     </div>
   )
