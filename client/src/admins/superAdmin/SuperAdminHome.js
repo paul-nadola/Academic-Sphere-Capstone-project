87faa47eca4950365eb746b2f25b5bdb6ec017c2
manotim <manotimike@gmail.com>
2023-08-04 01:55:35 +0300

    added navigation link to home from super admin page

--- client/src/admins/superAdmin/SuperAdminHome.js
+++ client/src/admins/superAdmin/SuperAdminHome.js
@@ -4,6 +4,12 @@ import { Link } from 'react-router-dom';
 function SuperAdmin() {
   return (
     <div>
+      <button className="mb-2">
+        <Link to="/" className='block bg-gray-200 text-blue-600 py-2 px-4 rounded hover:bg-gray-300 transition duration-300 w-full text-center'>
+        <button>Home</button>
+      </Link>
+        </button>
+        
         <button className='mt-4'>
             <Link
               to="/superadmin/teachers_data"
