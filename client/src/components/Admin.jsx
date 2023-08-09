72d91ce1dd056d3daaafbd2db64be829a2016917
manotim <manotimike@gmail.com>
2023-08-09 17:52:10 +0300

    added admin profile

--- client/src/components/Admin.jsx
+++ client/src/components/Admin.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+
 
 function Admin() {
+      const[currentUser, setCurrentUser] = useState([])
+    useEffect(() => {
+    const getUser = async () => {
+      try {
+        const token = sessionStorage.getItem('token');
+        console.log('Token used:', token);
+        const response = await fetch(
+          'https://academic-sphere-tables.onrender.com/admin_create',
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (!response.ok) {
+          console.error('Failed to fetch current user:', response.status);
+          return;
+        }
+
+        const data = await response.json();
+        setCurrentUser(data.current);
+      } catch (error) {
+        console.error('Error fetching teachers:', error);
+      }
+    };
+
+    getUser();
+  }, []);
   return (
-    <div>Admin</div>
+    <div>
+      <div className="flex justify-evenly">
+      <button className='mt-4'>
+            <Link
+              to="/admin/students_data"
+              className="block bg-pri2 text-white py-2 px-4 rounded hover:bg-pri1 transition duration-300 w-full text-center"
+            >
+              
+              Students Data
+            </Link>
+            </button>
+            
+      </div>
+      
+        <div className="user-container" >
+          <h2>ADMIN PROFILE</h2>
+          <h3>First Name: {currentUser.first_name}</h3>
+          <p>Last Name: {currentUser.last_name}</p>
+          <p>Phone Number: {currentUser.phone_number}</p>
+          <p>Date Of Birth: {currentUser.DOB}</p>
+        </div>
+    </div>
   )
 }
 
