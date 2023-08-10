3833aec644b5331ccf9d6dccb80fa171b0836475
Tomno <tomnoviv@gmail.com>
2023-08-10 19:21:48 +0300

    Student

--- client/src/components/StudentsPage.jsx
+++ client/src/components/StudentsPage.jsx
@@ -3,19 +3,48 @@ import { Link } from 'react-router-dom';
 
 function StudentsPage() {
   const [studentData, setStudentData] = useState(null);
+  const [assessmentResults, setAssessmentResults] = useState([]);
+  const [feeBalance, setFeeBalance] = useState({});
+  const [attendanceData, setAttendanceData] = useState([]);
 
   useEffect(() => {
-    
-    fetch('url')
-      .then(response => response.json()) 
+    fetch('studentUrl')
+      .then(response => response.json())
       .then(data => {
-        setStudentData(data); 
+        setStudentData(data);
       })
       .catch(error => {
         console.error('Error fetching student data:', error);
       });
-  }, []); 
-  
+
+    fetch('assessmentResultsUrl')
+      .then(response => response.json())
+      .then(data => {
+        setAssessmentResults(data);
+      })
+      .catch(error => {
+        console.error('Error fetching assessment results:', error);
+      });
+
+    fetch('feeBalanceUrl')
+      .then(response => response.json())
+      .then(data => {
+        setFeeBalance(data);
+      })
+      .catch(error => {
+        console.error('Error fetching fee balance:', error);
+      });
+
+    fetch('attendanceDataUrl')
+      .then(response => response.json())
+      .then(data => {
+        setAttendanceData(data);
+      })
+      .catch(error => {
+        console.error('Error fetching attendance data:', error);
+      });
+  }, []);
+
   return (
     <div>
       <div className="flex justify-evenly">
@@ -53,8 +82,8 @@ function StudentsPage() {
         </button>
       </div>
 
+
       <div className="p-4">
-        
         {studentData && (
           <div className="p-4 space-x-4">
             <div className="inline-block border rounded p-4 text-pri1">
@@ -67,20 +96,22 @@ function StudentsPage() {
               <h4><b>Instructor:</b> {studentData.instructor}</h4>
               <h4><b>Parent:</b> {studentData.parent}</h4>
             </div>
- 
+
             <div className="inline-block border rounded p-4 text-pri1">
               <div className='mb-5'>
-                
                 <h1><b>Attendance</b></h1>
-                {/* ... */}
+                <ul>
+                  {attendanceData.map((attendance, index) => (
+                    <li key={index}>{attendance.date}: {attendance.status}</li>
+                  ))}
+                </ul>
               </div>
             </div>
 
             <div className="inline-block border rounded p-4 text-pri1">
               <div className="mb-5">
-                
-                <h3><b>FEE BALANCE:</b> -5670</h3>
-                <h3>Fee Amount Paid: 54000</h3>
+                <h3><b>FEE BALANCE:</b> {feeBalance.balance}</h3>
+                <h3>Fee Amount Paid: {feeBalance.paid}</h3>
               </div>
             </div>
           </div>
@@ -90,27 +121,28 @@ function StudentsPage() {
       <hr />
 
       <div className="p-4 text-pri2">
-        <h3><b>Assessment Results for Christopher Jackson</b></h3>
-        
+        {studentData && (
+          <h3><b>Assessment Results for {studentData.name}</b></h3>
+        )}
         <table className="table-auto w-full mt-4 bg-white p-4 text-pri2">
-          <tr>
-            <th className="border px-4 py-2">Unit</th>
-            <th className="border px-4 py-2">Marks</th>
-            <th className="border px-4 py-2">Grade</th>
-            <th className="border px-4 py-2">Instructor Comments</th>
-          </tr>
-          <tr>
-            <td className="border px-4 py-2">Digital threats</td>
-            <td className="border px-4 py-2">83</td>
-            <td className="border px-4 py-2">A</td>
-            <td className="border px-4 py-2">Great work, keep it up</td>
-          </tr>
-          <tr>
-            <td className="border px-4 py-2">Ethical hacking</td>
-            <td className="border px-4 py-2">87</td>
-            <td className="border px-4 py-2">A</td>
-            <td className="border px-4 py-2">Very impressive</td>
-          </tr>
+          <thead>
+            <tr>
+              <th className="border px-4 py-2">Unit</th>
+              <th className="border px-4 py-2">Marks</th>
+              <th className="border px-4 py-2">Grade</th>
+              <th className="border px-4 py-2">Instructor Comments</th>
+            </tr>
+          </thead>
+          <tbody>
+            {assessmentResults.map((result, index) => (
+              <tr key={index}>
+                <td className="border px-4 py-2">{result.unit}</td>
+                <td className="border px-4 py-2">{result.marks}</td>
+                <td className="border px-4 py-2">{result.grade}</td>
+                <td className="border px-4 py-2">{result.comments}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </div>
